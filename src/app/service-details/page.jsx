3b75a5dc2ab22960175f108fea03ea0651c68dcd
ANKITispecia <ankit.ispecia@gmail.com>
2025-02-25 13:51:41 +0530

    Update page.jsx

--- src/app/service-details/page.jsx
+++ src/app/service-details/page.jsx
@@ -1,32 +1,28 @@
-// import Breadcrumb from "@/components/Breadcrumb";
+import Breadcrumb from "@/components/Breadcrumb";
 
-// import ServiceDetailsArea from "@/components/ServiceDetailsArea";
+import ServiceDetailsArea from "@/components/ServiceDetailsArea";
 
-
-
-
-
-// export const metadata = {
-//   title: "Service Details || AglieTech - IT Solutions & Technology NEXT JS Template",
-//   description:
-//     "Agiletech provide you to build the best agency, app, business, digital, it services, it solutions, network solution, startup, technology, technology company, technology service template.",
-// };
+export const metadata = {
+  title: "Service Details || AglieTech - IT Solutions & Technology NEXT JS Template",
+  description:
+    "Agiletech provide you to build the best agency, app, business, digital, it services, it solutions, network solution, startup, technology, technology company, technology service template.",
+};
 
 
-// const page = () => {
-//   return (
-//     <>
+const page = () => {
+  return (
+    <>
       
 
-//       {/* Navigation Bar */}
-//       <Breadcrumb title={"Service Details"} />
+      {/* Navigation Bar */}
+      <Breadcrumb title={"Service Details"} />
 
-//       {/* Service Details Area */}
-//       <ServiceDetailsArea />
+      {/* Service Details Area */}
+      <ServiceDetailsArea />
 
       
-//     </>
-//   );
-// };
+    </>
+  );
+};
 
-// export default page;
+export default page;
